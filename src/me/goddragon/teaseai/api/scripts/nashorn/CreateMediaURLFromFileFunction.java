87cfc02ea345d79f938b886ce2beb391a4769a3a
BuildTools <unconfigured@null.spigotmc.org>
2018-05-20 16:53:52 +0200

    Fixed: - Metronome not ending at the end of a session - Media URL creation failing because it added a strange xml tag Added: - Support to use custom urls from tumblr - Support to show files and media urls directly instead of just accepting a path - Set- and GetTypeSpeed to allow the user to manipulate the type speed of the dom

---
+++ src/me/goddragon/teaseai/api/scripts/nashorn/CreateMediaURLFromFileFunction.java
@@ -0,0 +1,7 @@
+package me.goddragon.teaseai.api.scripts.nashorn;
+
+/**
+ * Created by GodDragon on 20.05.2018.
+ */
+public class CreateMediaURLFromFileFunction {
+}
---
+++ src/me/goddragon/teaseai/api/scripts/nashorn/GetTypeSpeedFunction.java
@@ -0,0 +1,7 @@
+package me.goddragon.teaseai.api.scripts.nashorn;
+
+/**
+ * Created by GodDragon on 18.05.2018.
+ */
+public class GetTypeSpeedFunction {
+}
