d5908d27fb4b3ec5cad5d45ba4144f842273701a
BuildTools <unconfigured@null.spigotmc.org>
2018-06-14 22:22:13 +0200

    1.0.8 release

--- src/me/goddragon/teaseai/TeaseAI.java
+++ src/me/goddragon/teaseai/TeaseAI.java
@@ -31,7 +31,7 @@ import java.util.logging.Level;
  */
 public class TeaseAI extends Application {
 
-    public static final String VERSION = "1.0.7";
+    public static final String VERSION = "1.0.8";
 
     public static TeaseAI application;
     private ConfigHandler configHandler = new ConfigHandler("TeaseAI.properties");
