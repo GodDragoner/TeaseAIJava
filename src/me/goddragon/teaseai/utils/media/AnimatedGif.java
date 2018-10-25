136d9a1b5739822772b764bf764a1edfcfab2fe8
GodDragoner <unconfigured@null.spigotmc.org>
2018-10-26 01:33:27 +0200

    Changes

--- src/me/goddragon/teaseai/utils/media/AnimatedGif.java
+++ src/me/goddragon/teaseai/utils/media/AnimatedGif.java
@@ -1,10 +1,13 @@
 package me.goddragon.teaseai.utils.media;
 
-import javafx.embed.swing.SwingFXUtils;
 import javafx.scene.image.Image;
-import javafx.scene.image.WritableImage;
 
+import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * Created by GodDragon on 15.08.2018.
@@ -19,9 +22,20 @@ public class AnimatedGif extends Animation {
 
         Image[] sequence = new Image[d.getFrameCount()];
         for (int i = 0; i < d.getFrameCount(); i++) {
-            WritableImage wimg = null;
+            //WritableImage wimg = null;
             BufferedImage bimg = d.getFrame(i);
-            sequence[i] = SwingFXUtils.toFXImage(bimg, wimg);
+            //sequence[i] = SwingFXUtils.toFXImage(bimg, wimg);
+            ByteArrayOutputStream os = new ByteArrayOutputStream();
+
+            try {
+                ImageIO.write(bimg, "gif", os);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            InputStream is = new ByteArrayInputStream(os.toByteArray());
+            sequence[i] = new Image(is);
+
             durationMs += d.getDelay(i);
         }
 
