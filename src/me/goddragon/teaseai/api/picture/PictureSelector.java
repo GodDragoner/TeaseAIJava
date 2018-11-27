292927b4a85080dca8414271b65a6d8a3f9b27ed
Skier233 <benchy222@gmail.com>
2018-11-27 14:55:28 -0500

    Fixed domme image set, showTaggedImagesExact, and the scrolling issue in the main gui

--- src/me/goddragon/teaseai/api/picture/PictureSelector.java
+++ src/me/goddragon/teaseai/api/picture/PictureSelector.java
@@ -3,8 +3,13 @@ package me.goddragon.teaseai.api.picture;
 import me.goddragon.teaseai.TeaseAI;
 import me.goddragon.teaseai.api.chat.ChatParticipant;
 import me.goddragon.teaseai.api.session.Session;
+import me.goddragon.teaseai.utils.TeaseLogger;
 
+import java.io.File;
+import java.io.FilenameFilter;
+import java.util.Random;
 import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
 
 /**
  * Created by GodDragon on 26.03.2018.
@@ -20,18 +25,68 @@ public class PictureSelector {
         int preferredSessionDuration = TeaseAI.application.PREFERRED_SESSION_DURATION.getInt();
         double percentage = minutesPassed/preferredSessionDuration*100D;
 
-        if(percentage > 80) {
-            return participant.getPictureSet().getRandomPictureForStates(DressState.NAKED, DressState.SEE_THROUGH);
-        } else if(percentage > 60) {
-            return participant.getPictureSet().getRandomPictureForStates(DressState.HANDS_COVERING);
-        } else if(percentage > 40) {
-            return participant.getPictureSet().getRandomPictureForStates(DressState.GARMENT_COVERING);
-        } else if(percentage > 20) {
-            return participant.getPictureSet().getRandomPictureForStates(DressState.HALF_DRESSED);
-        } else if(percentage > 10) {
-            return participant.getPictureSet().getRandomPictureForStates(DressState.FULLY_DRESSED, DressState.HALF_DRESSED);
-        } else {
-            return participant.getPictureSet().getRandomPicture(DressState.FULLY_DRESSED, PictureTag.FACE);
+        if (percentage <= 10)
+        {
+            TaggedPicture toReturn = participant.getPictureSet().getRandomPicture(DressState.FULLY_DRESSED, PictureTag.FACE);
+            if (toReturn != null)
+            {
+                return toReturn;
+            }
+        }
+        if (percentage <= 20)
+        {
+            TaggedPicture toReturn = participant.getPictureSet().getRandomPictureForStates(DressState.FULLY_DRESSED, DressState.HALF_DRESSED);
+            if (toReturn != null)
+            {
+                return toReturn;
+            }
+        }
+        if (percentage <= 40)
+        {
+            TaggedPicture toReturn = participant.getPictureSet().getRandomPictureForStates(DressState.HALF_DRESSED);
+            if (toReturn != null)
+            {
+                return toReturn;
+            }
+        }
+        if (percentage <= 60)
+        {
+            TaggedPicture toReturn = participant.getPictureSet().getRandomPictureForStates(DressState.GARMENT_COVERING);
+            if (toReturn != null)
+            {
+                return toReturn;
+            }
+        }
+        if (percentage <= 80)
+        {
+            TaggedPicture toReturn = participant.getPictureSet().getRandomPictureForStates(DressState.HANDS_COVERING);
+            if (toReturn != null)
+            {
+                return toReturn;
+            }
+        }
+        TaggedPicture toReturn = participant.getPictureSet().getRandomPictureForStates(DressState.NAKED, DressState.SEE_THROUGH);
+        if (toReturn != null)
+        {
+            return toReturn;
+        }
+        else
+        {
+            File setFolder = participant.getPictureSet().getFolder();
+            File[] locFiles = setFolder.listFiles(new FilenameFilter() {
+                @Override
+                public boolean accept(File dir, String name) {
+                    return (name.toLowerCase().endsWith(".jpg") || name.toLowerCase().endsWith(".png") || name.toLowerCase().endsWith(".gif"));
+                }
+            });
+            if (locFiles.length == 0)
+            {
+                TeaseLogger.getLogger().log(Level.SEVERE, "Image set selected has no images!!");
+                return null;
+            }
+            
+            Random random = new Random();
+            return new TaggedPicture(locFiles[random.nextInt(locFiles.length)]);
         }
     }
 }
