9da4dc865549012ada5d36b58d1b29be6e717bdb
Skier233 <benchy222@gmail.com>
2018-12-27 23:50:22 -0500

    experimental release

--- src/me/goddragon/teaseai/utils/libraries/ripme/ripper/rippers/PornhubRipper.java
+++ src/me/goddragon/teaseai/utils/libraries/ripme/ripper/rippers/PornhubRipper.java
@@ -4,28 +4,31 @@ import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import me.goddragon.teaseai.utils.libraries.ripme.ripper.AbstractHTMLRipper;
+import me.goddragon.teaseai.utils.libraries.ripme.ripper.AlbumRipper;
+
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import me.goddragon.teaseai.utils.libraries.ripme.ripper.DownloadThreadPool;
+import me.goddragon.teaseai.utils.libraries.ripme.ui.RipStatusMessage.STATUS;
 import me.goddragon.teaseai.utils.libraries.ripme.utils.Http;
 import me.goddragon.teaseai.utils.libraries.ripme.utils.Utils;
 
-public class PornhubRipper extends AbstractHTMLRipper {
+public class PornhubRipper extends AlbumRipper {
     // All sleep times are in milliseconds
-    private static final int IMAGE_SLEEP_TIME    = 1000;
+    private static final int IMAGE_SLEEP_TIME    = 1  * 1000;
 
     private static final String DOMAIN = "pornhub.com", HOST = "Pornhub";
 
     // Thread pool for finding direct image links from "image" pages (html)
     private DownloadThreadPool pornhubThreadPool = new DownloadThreadPool("pornhub");
+
+    // Current HTML document
+    private Document albumDoc = null;
 
     public PornhubRipper(URL url) throws IOException {
         super(url);
@@ -36,63 +39,25 @@ public class PornhubRipper extends AbstractHTMLRipper {
         return HOST;
     }
 
-    @Override
-    protected String getDomain() {
-        return DOMAIN;
-    }
-
-    @Override
-    protected Document getFirstPage() throws IOException {
-        return Http.url(url).referrer(url).get();
-    }
-
-    @Override
-    public Document getNextPage(Document page) throws IOException {
-        Elements nextPageLink = page.select("li.page_next > a");
-        if (nextPageLink.isEmpty()){
-            throw new IOException("No more pages");
-        } else {
-            URL nextURL = new URL(this.url, nextPageLink.first().attr("href"));
-            return Http.url(nextURL).get();
-        }
-    }
-
-    @Override
-    protected List<String> getURLsFromPage(Document page) {
-        List<String> pageURLs = new ArrayList<>();
-        // Find thumbnails
-        Elements thumbs = page.select(".photoBlockBox li");
-        // Iterate over thumbnail images on page
-        for (Element thumb : thumbs) {
-            String imagePage = thumb.select(".photoAlbumListBlock > a")
-                    .first().attr("href");
-            String fullURL = "https://pornhub.com" + imagePage;
-            pageURLs.add(fullURL);
-        }
-        return pageURLs;
-    }
-
-    @Override
-    protected void downloadURL(URL url, int index) {
-        PornhubImageThread t = new PornhubImageThread(url, index, this.workingDir);
-        pornhubThreadPool.addThread(t);
-        try {
-            Thread.sleep(IMAGE_SLEEP_TIME);
-        } catch (InterruptedException e) {
-            LOGGER.warn("Interrupted while waiting to load next image", e);
-        }
-    }
-
     public URL sanitizeURL(URL url) throws MalformedURLException {
-        // always start on the first page of an album
-        // (strip the options after the '?')
-        String u = url.toExternalForm();
-        if (u.contains("?")) {
-            u = u.substring(0, u.indexOf("?"));
-            return new URL(u);
-        } else {
-            return url;
+        return url;
+    }
+
+    public String getAlbumTitle(URL url) throws MalformedURLException {
+        try {
+            // Attempt to use album title as GID
+            if (albumDoc == null) {
+                LOGGER.info("    Retrieving " + url.toExternalForm());
+                sendUpdate(STATUS.LOADING_RESOURCE, url.toString());
+                albumDoc = Http.url(url).get();
+            }
+            Elements elems = albumDoc.select(".photoAlbumTitleV2");
+            return HOST + "_" + elems.get(0).text();
+        } catch (Exception e) {
+            // Fall back to default album naming convention
+            LOGGER.warn("Failed to get album title from " + url, e);
         }
+        return super.getAlbumTitle(url);
     }
 
     @Override
@@ -100,7 +65,7 @@ public class PornhubRipper extends AbstractHTMLRipper {
         Pattern p;
         Matcher m;
 
-        p = Pattern.compile("^.*pornhub\\.com/album/([0-9]+).*$");
+        p = Pattern.compile("^.*pornhub\\.com/album/([0-9]+)$");
         m = p.matcher(url.toExternalForm());
         if (m.matches()) {
             return m.group(1);
@@ -113,8 +78,48 @@ public class PornhubRipper extends AbstractHTMLRipper {
     }
 
     @Override
-    public DownloadThreadPool getThreadPool(){
-        return pornhubThreadPool;
+    public void rip() throws IOException {
+        int index = 0;
+        String nextUrl = this.url.toExternalForm();
+
+        if (albumDoc == null) {
+            LOGGER.info("    Retrieving album page " + nextUrl);
+            sendUpdate(STATUS.LOADING_RESOURCE, nextUrl);
+            albumDoc = Http.url(nextUrl)
+                           .referrer(this.url)
+                           .get();
+        }
+
+        // Find thumbnails
+        Elements thumbs = albumDoc.select(".photoBlockBox li");
+        if (thumbs.size() == 0) {
+            LOGGER.debug("albumDoc: " + albumDoc);
+            LOGGER.debug("No images found at " + nextUrl);
+            return;
+        }
+
+        // Iterate over images on page
+        for (Element thumb : thumbs) {
+            if (isStopped()) {
+                break;
+            }
+            index++;
+            String imagePageUrl = thumb.select(".photoAlbumListBlock > a").first().attr("href");
+            URL imagePage = new URL(url, imagePageUrl);
+            PornhubImageThread t = new PornhubImageThread(imagePage, index, this.workingDir);
+            pornhubThreadPool.addThread(t);
+            if (isThisATest()) {
+                break;
+            }
+            try {
+                Thread.sleep(IMAGE_SLEEP_TIME);
+            } catch (InterruptedException e) {
+                LOGGER.warn("Interrupted while waiting to load next image", e);
+            }
+        }
+
+        pornhubThreadPool.waitForThreads();
+        waitForThreads();
     }
 
     public boolean canRip(URL url) {
@@ -130,7 +135,7 @@ public class PornhubRipper extends AbstractHTMLRipper {
         private URL url;
         private int index;
 
-        PornhubImageThread(URL url, int index, File workingDir) {
+        public PornhubImageThread(URL url, int index, File workingDir) {
             super();
             this.url = url;
             this.index = index;
