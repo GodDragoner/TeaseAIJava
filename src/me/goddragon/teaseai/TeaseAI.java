7d99f3a3758636e65ad18fa9be66b614d3566fc2
GodDragoner <unconfigured@null.spigotmc.org>
2019-04-01 02:41:10 +0200

    1.1 release

--- src/me/goddragon/teaseai/TeaseAI.java
+++ src/me/goddragon/teaseai/TeaseAI.java
@@ -39,7 +39,7 @@ import java.util.logging.Level;
  */
 public class TeaseAI extends Application {
 
-    public static final String VERSION = "1.0.18";
+    public static final String VERSION = "1.1";
     public static final String UPDATE_FOLDER = "Updates";
 
 
