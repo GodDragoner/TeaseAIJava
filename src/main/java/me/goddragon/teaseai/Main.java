d2ff78b22168fcdacffe0fb7648f32ce9eb6e67f
morgenkaffe <morgenkaffe3@users.noreply.github.com>
2019-11-22 19:27:58 +0100

    Remove old javafx download logic

--- src/main/java/me/goddragon/teaseai/Main.java
+++ src/main/java/me/goddragon/teaseai/Main.java
@@ -20,107 +20,11 @@ public class Main {
 
     public static void main(String[] args) {
         TeaseLogger.getLogger().log(Level.INFO, "Checking libraries for updates...");
-       // TODO Reimplement UpdateHandler.getHandler().checkLibraries();
+       // TODO Reimplement 
+        UpdateHandler.getHandler().checkLibraries();
         TeaseLogger.getLogger().log(Level.INFO, "Libraries checked and up-to-date.");
-
-        if (false /* TODO Not necessary anymore; included during build for build platform */&& ManagementFactory.getRuntimeMXBean().getInputArguments().size() == 0 && JAVA_VERSION > 10) {
-            try {
-                //Re-launch the app itself with VM option passed
-                File currentDir = Paths.get(System.getProperty("user.dir")).toFile();
-
-                if (getJavaFXLibFolder() == null) {
-
-                    TeaseLogger.getLogger().log(Level.SEVERE, "No JavaFX installation found. Asking for download...");
-
-                    String fileName = "openJFX";
-
-                    if (!new File(fileName + ".zip").exists()) {
-                        int dialogButton = JOptionPane.YES_NO_OPTION;
-                        int dialogResult = JOptionPane.showConfirmDialog(null, "No OpenJFX installation found. Would you like to download?", "OpenJFX", dialogButton);
-
-                        if (dialogResult != JOptionPane.YES_OPTION) {
-                            JOptionPane.showMessageDialog(null, "Can't run on Java 11 or higher without OpenJFX. Exiting...");
-                            System.exit(0);
-                            return;
-                        }
-
-                        if (!System.getProperty("os.arch").contains("64")) {
-                            JOptionPane.showMessageDialog(null, "x86 systems are currently not supported by the auto updater." +
-                                    " Please fetch your own version of OpenFX from https://gluonhq.com/products/javafx/");
-                            System.exit(0);
-                            return;
-                        }
-
-                        String downloadPath;
-                        if (isWindows()) {
-                            TeaseLogger.getLogger().log(Level.SEVERE, "Your running Windows. Fetching OpenJFX SDK...");
-                            downloadPath = "https://github.com/GodDragoner/TeaseAIJava/raw/master/Resources/openjfx_windows-x64_bin-sdk.zip";
-                        } else if (isMac()) {
-                            TeaseLogger.getLogger().log(Level.SEVERE, "Your running MacOS. Fetching OpenJFX SDK...");
-                            downloadPath = "https://github.com/GodDragoner/TeaseAIJava/raw/master/Resources/openjfx_osx-x64_bin-sdk.zip";
-                        } else if (isUnix()) {
-                            TeaseLogger.getLogger().log(Level.SEVERE, "Your running Linux/Unix. Fetching OpenJFX SDK...");
-                            downloadPath = "https://github.com/GodDragoner/TeaseAIJava/raw/master/Resources/openjfx_linux-x64_bin-sdk.zip";
-                        } else {
-                            JOptionPane.showMessageDialog(null, "Your OS is not supported by JavaFX yet! Exiting.");
-                            System.exit(0);
-                            return;
-                        }
-
-
-                        URL url = new URL(downloadPath);
-                        HttpURLConnection httpConnection = (HttpURLConnection) (url.openConnection());
-                        long completeFileSize = httpConnection.getContentLength();
-
-                        ProgressMonitor progressMonitor = new ProgressMonitor(null, "Downloading OpenJFX...", "", 0, (int) completeFileSize);
-
-                        BufferedInputStream in = new BufferedInputStream(httpConnection.getInputStream());
-                        FileOutputStream fos = new FileOutputStream(fileName + ".zip");
-                        BufferedOutputStream bout = new BufferedOutputStream(fos, 1024);
-
-                        byte[] data = new byte[1024];
-                        long downloadedFileSize = 0;
-                        int x;
-                        //int oldProgress = 0;
-
-                        while ((x = in.read(data, 0, 1024)) >= 0) {
-                            downloadedFileSize += x;
-
-                            //Calculate progress
-                            //final int currentProgress = (int) ((((double) downloadedFileSize) / ((double) completeFileSize)) * 100d);
-
-                            progressMonitor.setProgress((int) downloadedFileSize);
-
-                            /*if (currentProgress > oldProgress) {
-                                TeaseLogger.getLogger().log(Level.INFO, "Download Progress at " + currentProgress + "%");
-                                oldProgress = currentProgress;
-                            }*/
-
-                            bout.write(data, 0, x);
-                        }
-
-                        bout.close();
-                        in.close();
-                    }
-
-                    TeaseLogger.getLogger().log(Level.INFO, "Finished downloading OpenJFX. Unzipping...");
-
-                    //Unzip the downloaded file
-                    ZipUtils.unzipFile(new File(fileName + ".zip"), currentDir);
-                    //Delete the downloaded zip file
-                    //newUpdateZipFile.delete();
-                }
-
-            } catch (IOException ioe) {
-                ioe.printStackTrace();
-            }
-
-            TeaseLogger.getLogger().log(Level.INFO, "Restarting TAJ with JAVA-FX startup parameters...");
-            restart();
-        } else {
-            TeaseLogger.getLogger().log(Level.INFO, "Initialization done.");
-            TeaseAI.main(args);
-        }
+        TeaseLogger.getLogger().log(Level.INFO, "Initialization done.");
+        TeaseAI.main(args);
     }
 
 
