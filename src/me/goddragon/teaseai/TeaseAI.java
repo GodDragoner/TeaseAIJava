bcecc5f8c4345e8f25f25b9cdb153e808e55ad9c
GodDragoner <unconfigured@null.spigotmc.org>
2020-03-03 20:09:48 +0100

    1.2.1

--- src/me/goddragon/teaseai/TeaseAI.java
+++ src/me/goddragon/teaseai/TeaseAI.java
@@ -43,7 +43,7 @@ import java.util.logging.Level;
 public class TeaseAI extends Application {
 
 
-    public static final String VERSION = "1.2";
+    public static final String VERSION = "1.2.1";
     public static final String UPDATE_FOLDER = "Updates";
 
     public static TeaseAI application;
