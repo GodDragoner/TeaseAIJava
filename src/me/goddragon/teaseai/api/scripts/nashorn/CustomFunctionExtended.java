ccc8bbfdb1a8082d04e3a560740d8689ccf07636
FrozenWolf <FrozenWolf4887@yahoo.com>
2021-01-16 22:30:51 +0000

    Refactor ShowImageFunction to reduce complexity
    The new base class, CustomFunctionExtended, uses reflection to determine
    the appropriate onCall(...) method in the derived class to be invoked
    based on the argument types passed to call().  This greatly simplifies
    the implementation in the derived class that needs only to provide the
    various onCall(...) methods with the required parameter types.
    
    When the base class is unable to match an appropriate onCall(...) method
    in the derived class because there is no parameter type match, log
    information is generated to support the developer of the personality.
    For example if there is a call to showImage(10, "myImage.jpg"), the
    following log is produced:
    "
      06:54:28 pm SEVERE: No match for function call showImage(Integer, String)
      06:54:28 pm INFO: Candidate functions are:
      06:54:28 pm INFO:     showImage(File)
      06:54:28 pm INFO:     showImage(File, Integer)
      06:54:28 pm INFO:     showImage(MediaURL)
      06:54:28 pm INFO:     showImage(MediaURL, Integer)
      06:54:28 pm INFO:     showImage(String)
      06:54:28 pm INFO:     showImage(String, Integer)
    "
    
    CustomFunctionExtended derives from the existing CustomFunction so that
    all of the existing functions that derive from CustomFunction are
    unaffected by this change.  If desired, the other functions could also
    be modified in the same way, one at a time if needed.  If all of the
    functions are eventually modified to derive from CustomFunctionExtended,
    then CustomFunction and CustomFunctionExtended could be merged together.

---
+++ src/me/goddragon/teaseai/api/scripts/nashorn/CustomFunctionExtended.java
@@ -0,0 +1,104 @@
+package me.goddragon.teaseai.api.scripts.nashorn;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+
+import me.goddragon.teaseai.utils.TeaseLogger;
+
+public abstract class CustomFunctionExtended extends CustomFunction {
+    private static final String HANDLER_METHOD_NAME = "onCall";
+
+    protected CustomFunctionExtended(String... functionName) {
+        super(functionName);
+    }
+
+    @Override
+    public Object call(Object object, Object... args) {
+        super.call(object, args);
+
+        final Class<?>[] argTypeList = objectListToTypeList(args);
+
+        Object result = null;
+        String faultMessage = null;
+
+        try {
+            final Method method = getClass().getDeclaredMethod(HANDLER_METHOD_NAME, argTypeList);
+            result = method.invoke(this, args);
+        } catch (NoSuchMethodException e) {
+            faultMessage = "No match for";
+        } catch (SecurityException e) {
+            faultMessage = "Inaccessible";
+        } catch (IllegalAccessException e) {
+            faultMessage = "Illegal access to";
+        } catch (IllegalArgumentException e) {
+            faultMessage = "Illegal argument passed to";
+        } catch (InvocationTargetException e) {
+            faultMessage = "Invocation exception during";
+        }
+
+        if (faultMessage != null) {
+            TeaseLogger.getLogger().log(Level.SEVERE,
+                    String.format("%s function call %s(%s)", faultMessage, getFunctionName(),
+                            typeListToString(argTypeList)));
+
+            logCandidateFunctions();
+        }
+
+        return result;
+    }
+
+    private Class<?>[] objectListToTypeList(Object[] args) {
+        final Class<?>[] typeList = new Class<?>[ args.length ];
+        for (int i = 0; i < args.length; ++i) {
+            typeList[i] = args[i].getClass();
+        }
+
+        return typeList;
+    }
+
+    private void logCandidateFunctions() {
+        TeaseLogger.getLogger().log(Level.INFO, "Candidate functions are:");
+
+        final List<String> listOfEachMethodsArguments = new ArrayList<>();
+        for (Method method : getClass().getDeclaredMethods()) {
+            if (method.getName().contentEquals(HANDLER_METHOD_NAME) && method.canAccess(this)) {
+                listOfEachMethodsArguments.add(typeListToString(method.getParameterTypes()));
+            }
+        }
+
+        listOfEachMethodsArguments.sort(String::compareTo);
+
+        for (String methodArguments : listOfEachMethodsArguments) {
+            TeaseLogger.getLogger().log(
+                    Level.INFO, String.format("    %s(%s)", getFunctionName(), methodArguments));
+        }
+    }
+
+    private String typeListToString(Class<?>[] typeList) {
+        StringBuilder text = new StringBuilder();
+        boolean isFirstParameter = true;
+
+        for (Class<?> pType : typeList) {
+            if (isFirstParameter)
+                isFirstParameter = false;
+            else
+                text.append(", ");
+
+            text.append(getSimplifiedTypeName(pType.getName()));
+        }
+
+        return text.toString();
+    }
+
+    private String getSimplifiedTypeName(String typeName) {
+        final int lastSplit = typeName.lastIndexOf('.');
+        if (lastSplit != -1) {
+            return typeName.substring(lastSplit + 1);
+        } else {
+            return typeName;
+        }
+    }
+}
