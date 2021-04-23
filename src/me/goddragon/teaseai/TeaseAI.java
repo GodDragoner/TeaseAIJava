3ced2514edb837495ae81c8fb0297dc0d3acc36b
GodDragoner <goddragoner@gmail.com>
2021-04-24 01:22:54 +0200

    1.4 release

--- src/me/goddragon/teaseai/TeaseAI.java
+++ src/me/goddragon/teaseai/TeaseAI.java
@@ -42,7 +42,7 @@ import java.util.logging.Level;
 public class TeaseAI extends Application {
 
 
-    public static final String VERSION = "1.3";
+    public static final String VERSION = "1.4";
     public static final String UPDATE_FOLDER = "Updates";
 
     public static TeaseAI application;
